4f4c0b0c7c7a6a4b2b16ad54f494ecf79332b2d7
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-15 22:10:04 +0300

    add firebase to project

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> init() async {
 
+  //
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
