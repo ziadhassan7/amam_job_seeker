e2fffe638b2b643a44bce837823e1a314cae9b80
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-17 06:13:59 +0300

    add new colors

--- lib/core/styles/app_colors.dart
+++ lib/core/styles/app_colors.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Colors.black;
   static const Color primaryLight = Colors.black54;
+
   static const Color accent = Color.fromRGBO(26, 188, 0, 1);
+  static const Color accentDark = Color.fromRGBO(34, 89, 21, 1.0);
+  static const Color accentBackground = Color.fromRGBO(237, 255, 233, 1.0);
 }
