c70caa45973a25698f1ea1b84dce5f4186eb9e74
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-17 04:02:14 +0300

    Add: displayName, email functions

---
+++ lib/core/firebase_auth/auth.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  ///                                                                           / Auth
+  //Login
+  Future<void> login({
+    required String email,
+    required String password}) async {
+
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  //Register
+  Future<void> registerUser({
+    required String name,
+    required String email,
+    required String password}) async {
+
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    await updateDisplayName(name);
+  }
+
+  //Logout
+  Future<void> logout({
+    required String email,
+    required String password}) async {
+
+    await _firebaseAuth.signOut();
+  }
+
+  ///                                                                           / User - Name
+  //Display name
+  Future<void> updateDisplayName(String name) async {
+    await currentUser!.updateDisplayName(name);
+  }
+
+  //Get display name
+  get displayName => currentUser!.displayName;
+
+
+  ///                                                                           / User - Email
+  //Display name
+  Future<void> updateUserEmail(String email) async {
+    await currentUser!.updateEmail(email);
+  }
+
+  //Get display name
+  get userEmail => currentUser!.email;
+}
