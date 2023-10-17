9e104f561d026c5825cc45797f180a73570579c7
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-17 05:18:23 +0300

    Initial Page that redirects you when you're authenticated

---
+++ lib/initial_page.dart
@@ -0,0 +1,29 @@
+import 'package:amam_job_seeker_assessment/futures/auth/presentation/pages/login_page.dart';
+import 'package:amam_job_seeker_assessment/futures/profile/presentation/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'core/firebase_auth/auth.dart';
+
+class InitialPage extends StatefulWidget {
+  const InitialPage({super.key});
+
+  @override
+  State<InitialPage> createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          return const ProfilePage();
+        } else {
+          return const LoginPage();
+        }
+
+      }
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,19 @@
-import 'package:amam_job_seeker_assessment/futures/auth/presentation/pages/login_page.dart';
+import 'package:amam_job_seeker_assessment/initial_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'injection.dart' as inject;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   //initialization
   await inject.init();
 
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  late Widget startScreen;
-
-  ///Make App Ready - before building screen
-  @override
-  void initState() {
-    startScreen = const LoginPage();
-    super.initState();
-  }
-
-  ///Build Screen
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,7 +22,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
         useMaterial3: true,
       ),
-      home: startScreen,
+      home: const InitialPage(),
     );
   }
 }
