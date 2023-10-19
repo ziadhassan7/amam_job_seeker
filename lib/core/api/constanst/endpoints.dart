498083ecfb281f2b426e7668b21391580840c912
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-19 19:47:30 +0300

    add SuperParser Endpoints

--- lib/core/api/constanst/endpoints.dart
+++ lib/core/api/constanst/endpoints.dart
@@ -1,4 +1,14 @@
+///openWeather
+//Url
 const String openWeatherBaseUrl = "https://api.openweathermap.org";
-
+//Endpoints
 const String weatherEndpoint = "/data/2.5/weather";
 const String geoEndpoint = "/geo/1.0/direct";
+
+
+///superparser - resume parser
+//Url
+const String superParserBaseUrl = "https://api.superparser.com";
+//Endpoints
+const String parseEndpoint = "/parse";
+const String urlEndpoint = "/url";
