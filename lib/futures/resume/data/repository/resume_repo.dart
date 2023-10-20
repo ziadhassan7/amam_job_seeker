590f1844651aff01a0524ff44aa4ed1fb993c6d1
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-20 16:41:05 +0300

    add url parser method

--- lib/futures/resume/data/repository/resume_repo.dart
+++ lib/futures/resume/data/repository/resume_repo.dart
@@ -1,28 +1,55 @@
 import 'dart:io';
-import 'package:amam_job_seeker_assessment/core/utils/file_helper.dart';
 import 'package:dio/dio.dart';
 import '../../../../core/api/api_provider.dart';
 import '../../../../core/api/constanst/api_key.dart';
 import '../../../../core/api/constanst/endpoints.dart';
 import '../../../../core/custom_log.dart';
+import 'package:http_parser/http_parser.dart';
 
 class ResumeRepo {
 
   /// Use Weather Api
-  DioClient dio = DioClient(superParserBaseUrl);
+  static final DioClient _dio = DioClient(superParserBaseUrl);
 
-  // Get Today's weather from location
-  Future<Map?> parseResume({required File file}) async {
+  // Get resume data from file
+  static Future<Map?> parseResume({required File file}) async {
+
+    final formData = FormData.fromMap({
+      'file_name': await MultipartFile.fromFile(
+          file.path,
+          filename: file.path.split('/').last,
+          contentType: MediaType('application', 'pdf'), //MediaType.parse(lookupMimeType(file.path))
+      ),
+    });
 
     try {
-      Response response = await dio.post(
-          weatherEndpoint,
-          options: Options(
-            headers: {
-              "X-API-Key" : superParserApiKey
-            }
-          ),
-          data: FileHelper.getStringBytesFromFile(file),
+      Response response = await _dio.post(
+          parseEndpoint,
+          options: Options(headers: {"X-API-Key" : superParserApiKey}),
+          data: formData,
+      );
+
+      return response.data;
+
+    } catch (e) {
+      Log("SuperParser Exception:", e);
+      return null;
+    }
+  }
+
+  // Get resume data from link
+  static Future<Map?> parseUrlResume({required String resumeUrl}) async {
+
+
+    Log.p(resumeUrl,);
+
+    try {
+      Response response = await _dio.post(
+        urlEndpoint,
+        options: Options(
+            validateStatus: (status) => true,
+            headers: {"X-API-Key" : superParserApiKey,}),
+        data: {"resume_url": resumeUrl},
       );
 
       return response.data;
