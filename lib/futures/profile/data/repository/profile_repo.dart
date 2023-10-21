da5e710c9287ddfdb67173e18164260ee5e0dbf2
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-21 21:59:46 +0300

    polish code

--- lib/futures/profile/data/repository/profile_repo.dart
+++ lib/futures/profile/data/repository/profile_repo.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:amam_job_seeker_assessment/core/custom_log.dart';
 import 'package:amam_job_seeker_assessment/futures/profile/data/model/profile_model.dart';
 import '../../../../core/firebase/real_time/database_client.dart';
 
@@ -14,8 +13,6 @@ class ProfileRepo {
     final data=  await DatabaseClient.getOnce(dataPath: "$userId/jsonData");
 
     if(data != null){
-      Log("Firebase Data: ", data);
-      Log("Firebase Data (json): ", jsonDecode(data.toString()));
       ProfileModel model = ProfileModel.fromJson(jsonDecode(data.toString()));
       return model;
     }
