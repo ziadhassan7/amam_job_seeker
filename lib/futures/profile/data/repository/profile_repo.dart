434d1228e20378e01bd624964e787bfa0e6ba64f
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-21 06:51:57 +0300

    add data with user id

--- lib/futures/profile/data/repository/profile_repo.dart
+++ lib/futures/profile/data/repository/profile_repo.dart
@@ -1,6 +1,9 @@
+import '../../../../core/firebase/real_time/database_client.dart';
 
 class ProfileRepo {
 
-
+  static addProfile(Map data, String userId){
+    DatabaseClient.add(data, dataPath: userId);
+  }
 
 }
